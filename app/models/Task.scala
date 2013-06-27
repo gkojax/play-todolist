73b7f40a32913e4fe2c57d942bdcc8309ea89cb1
gkojax <gkojax@yahoo.com>
2013-06-27 09:30:37 +0900

    change to scalikejdbc

--- app/models/Task.scala
+++ app/models/Task.scala
@@ -1,41 +1,40 @@
 package models
 
-import anorm._
-import anorm.SqlParser._
-import play.api.db._
 import play.api.Play.current
 import scala.language.postfixOps
+import org.joda.time._
+import scalikejdbc._
 
-case class Task(id: Long, label: String, ymd: Option[org.joda.time.DateTime])
+case class Task(
+  id: Long,
+  label: String,
+  ymd: Option[DateTime] = None
+)
 
 object Task {
-  
-  val task = {
-    get[Long]("id") ~ 
-    get[String]("label") ~
-    get[org.joda.time.DateTime]("ymd") map {
-      case id~label~ymd => Task(id, label, ymd)
-    }
+  val task = (rs: WrappedResultSet) => Task(
+    id = rs.long("id"), 
+    label = rs.string("label"), 
+    ymd = rs.timestampOpt("tmd").map(_.toDateTime)
+  )
+
+  def all()(implicit session: DBSession = AutoSession): List[Task] = {
+    SQL("select * from task").map(task).list.apply()
   }
 
-  def all(): List[Task] = DB.withConnection { implicit c =>
-    SQL("select * from task").as(task *)
-  }
-  
   def create(task: Task) {
-    DB.withConnection { implicit c =>
-      SQL("insert into task (label) values ({label})").on(
-        'label -> task.label
-      ).executeUpdate()
-    }
+    // DB.withConnection { implicit c =>
+    //   SQL("insert into task (label) values ({label})").on(
+    //     'label -> task.label
+    //   ).executeUpdate()
+    // }
   }
 
   def delete(id: Long) {
-    DB.withConnection { implicit c =>
-      SQL("delete from task where id = {id}").on(
-        'id -> id
-      ).executeUpdate()
-    }
+    // DB.withConnection { implicit c =>
+    //   SQL("delete from task where id = {id}").on(
+    //     'id -> id
+    //   ).executeUpdate()
+    // }
   }
 }
-
--- project/Build.scala
+++ project/Build.scala
@@ -4,17 +4,21 @@ import play.Project._
 
 object ApplicationBuild extends Build {
 
-  val appName         = "todolist"
-  val appVersion      = "1.1"
+  val appName         = "play-todolist"
+  val appVersion      = "1.2"
 
   val appDependencies = Seq(
     // Add your project dependencies here,
     jdbc,
-    anorm
-    )
+    anorm,
+    "com.github.seratch" %% "scalikejdbc" % "[1.6,)",
+    "com.github.seratch" %% "scalikejdbc-play-plugin" % "[1.6,)"
+  )
+
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
     // Add your own project settings here      
     scalacOptions ++= Seq("-deprecation", "-feature")
   )
+
 }
